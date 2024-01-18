67c67f77e9882d085ca2165c7a3af04911f368c8
Hari Bhatta <haree5bhatta@gmail.com>
2024-01-18 13:00:29 +1100

    personal-portfolio

--- js/custom.js
+++ js/custom.js
@@ -1,36 +1,72 @@
 (function ($) {
-
   "use strict";
 
-    // COLOR MODE
-    $('.color-mode').click(function(){
-        $('.color-mode-icon').toggleClass('active')
-        $('body').toggleClass('dark-mode')
-    })
+  // COLOR MODE
+  $(".color-mode").click(function () {
+    $(".color-mode-icon").toggleClass("active");
+    $("body").toggleClass("dark-mode");
+  });
 
-    // HEADER
-    $(".navbar").headroom();
+  // HEADER
+  $(".navbar").headroom();
 
-    // PROJECT CAROUSEL
-    $('.owl-carousel').owlCarousel({
-    	items: 1,
-	    loop:true,
-	    margin:10,
-	    nav:true
-	});
+  // PROJECT CAROUSEL
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true,
+  });
 
-    // SMOOTHSCROLL
-    $(function() {
-      $('.nav-link, .custom-btn-link').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });  
+  // SMOOTHSCROLL
+  $(function () {
+    $(".nav-link, .custom-btn-link").on("click", function (event) {
+      var $anchor = $(this);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr("href")).offset().top - 49,
+          },
+          1000
+        );
+      event.preventDefault();
+    });
+  });
 
-    // TOOLTIP
-    $('.social-links a').tooltip();
-
+  // TOOLTIP
+  $(".social-links a").tooltip();
 })(jQuery);
+
+document.addEventListener("DOMContentLoaded", function () {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  }
+
+  scrollToTopBtn.addEventListener("click", function () {
+    smoothScrollTop();
+  });
+
+  function smoothScrollTop() {
+    var currentPosition =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentPosition > 0) {
+      window.requestAnimationFrame(smoothScrollTop);
+      window.scrollTo(0, currentPosition - currentPosition / 8);
+    }
+  }
+});
